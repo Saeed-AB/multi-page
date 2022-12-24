5dcb35701eb2d4f4c8d98bfcd4112e37288944c6
Saeed beraia <saeed@jisr.net>
2022-12-24 11:44:44 +0300

    add icon types

--- types/index.d.ts
+++ types/index.d.ts
@@ -21,3 +21,8 @@ export type FormStateTypes = {
   selectedOnes: string[];
 };
 
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.svg';
